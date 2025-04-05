08b32abd163d9562bb0e947a5becfbd0f9d60850
Dylan Travers <dtravers89@gmail.com>
2025-04-05 15:09:48 -0400

    refactor: move connection code to its own class and method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,12 +4,8 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/";
-        String username = "root";
-        String password = "pass";
-
+        Connection connection = MySQLConnectionUtility.getConnection();
         try {
-            Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println(connection.isValid(5));
         } catch (SQLException e) {
             // TODO Auto-generated catch block
---
+++ src/main/java/com/example/MySQLConnectionUtility.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnectionUtility {
+        static String url = "jdbc:mysql://localhost:3306/";
+        static String username = "root";
+        static String password = "pass";
+
+        static Connection getConnection() {
+            Connection connection = null;
+
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return connection;
+        }
+}
